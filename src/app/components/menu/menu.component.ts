4bd2cbc9196a566d60b567abb257d8bc478c2593
zrangonazaire <astairenazaire@gmail.com>
2025-03-05 11:28:17 +0000

    up

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+}
