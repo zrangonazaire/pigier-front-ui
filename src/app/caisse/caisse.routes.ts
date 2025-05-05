c1ecad8c44c1fc2e3615bfd546342fab88d19d70
zrangonazaire <61186442+zrangonazaire@users.noreply.github.com>
2025-05-05 10:46:38 +0000

    gestion pedagogique

---
+++ src/app/caisse/caisse.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { JournalCaisseComponent } from './journal-caisse/journal-caisse.component';
+
+export const CAISSE_ROUTES: Route[] = [
+  { path: 'journal-caisse', component: JournalCaisseComponent },
+];
