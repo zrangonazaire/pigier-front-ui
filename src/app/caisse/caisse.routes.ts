3515b0439f5567c58cef90056f0063e1be5e37af
bigbossoh <ange0145@gmail.com>
2025-05-06 14:17:20 +0000

    point-caisse

--- src/app/caisse/caisse.routes.ts
+++ src/app/caisse/caisse.routes.ts
@@ -1,6 +1,8 @@
 import { Route } from '@angular/router';
 import { JournalCaisseComponent } from './journal-caisse/journal-caisse.component';
+import { PointCaisseComponent } from './point-caisse/point-caisse.component';
 
 export const CAISSE_ROUTES: Route[] = [
   { path: 'journal-caisse', component: JournalCaisseComponent },
+  { path: 'point-caisse', component: PointCaisseComponent },
 ];
---
+++ src/app/caisse/point-caisse/point-caisse.component.ts
@@ -0,0 +1,69 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-point-caisse',
+  standalone: true,
+  // imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+
+    FormsModule,
+    CommonModule
+  ],
+  templateUrl: './point-caisse.component.html',
+  styleUrl: './point-caisse.component.scss'
+})
+export class PointCaisseComponent {
+  selectedCampuses: string[] = ['ABIDJAN PLATEAU']; // Tableau pour stocker les sélections multiples
+  selectedEncaissement: string = 'Tous';
+  selectedFiltre: string = 'Périodique';
+  selectedModeReglement: string = 'Tout';
+  caisseNumber: number = 4;
+  selectedDate: string = '2025-04-29';
+
+  campuses = [
+    'ABIDJAN PLATEAU',
+    'FOAD',
+    'YAMOUSSOUKRO',
+    'ABIDJAN YOPOUGON'
+  ];
+
+  encaissements = [
+    'Accessoire',
+    'Frais examen',
+    'Frais de scolarité',
+    'Société',
+    'Tous'
+  ];
+
+  filtres = [
+    'Périodique',
+    'Par folio',
+    'Grouper par folio'
+  ];
+
+  modesReglement = [
+    'Espèce',
+    'Chèque',
+    'Virement',
+    'Mobile money',
+    'Tout'
+  ];
+
+  toggleCampusSelection(campus: string): void {
+    const index = this.selectedCampuses.indexOf(campus);
+    if (index === -1) {
+      this.selectedCampuses.push(campus);
+    } else {
+      this.selectedCampuses.splice(index, 1);
+    }
+  }
+
+  isCampusSelected(campus: string): boolean {
+    return this.selectedCampuses.includes(campus);
+  }
+
+  
+}
