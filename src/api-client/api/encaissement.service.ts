e76aa043fd493c199ddfca1c74f0435a5dbf8395
bigbossoh <ange0145@gmail.com>
2025-09-01 09:21:36 +0000

    ok

--- src/api-client/api/encaissement.service.ts
+++ src/api-client/api/encaissement.service.ts
@@ -16,6 +16,8 @@ import { HttpClient, HttpHeaders, HttpParams,
 import { CustomHttpParameterCodec }                          from '../encoder';
 import { Observable }                                        from 'rxjs';
 
+// @ts-ignore
+import { EncaissementDTO } from '../model/encaissementDTO';
 
 // @ts-ignore
 import { BASE_PATH, COLLECTION_FORMATS }                     from '../variables';
@@ -31,6 +33,158 @@ export class EncaissementService extends BaseService {
 
     constructor(protected httpClient: HttpClient, @Optional() @Inject(BASE_PATH) basePath: string|string[], @Optional() configuration?: Configuration) {
         super(basePath, configuration);
+    }
+
+    /**
+     * @param etablissementSourceParam 
+     * @param paramDateDebut 
+     * @param paramDateFin 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public generateEtatFacturationReport(etablissementSourceParam: Array<string>, paramDateDebut: string, paramDateFin: string, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<Array<EncaissementDTO>>;
+    public generateEtatFacturationReport(etablissementSourceParam: Array<string>, paramDateDebut: string, paramDateFin: string, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<HttpResponse<Array<EncaissementDTO>>>;
+    public generateEtatFacturationReport(etablissementSourceParam: Array<string>, paramDateDebut: string, paramDateFin: string, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<HttpEvent<Array<EncaissementDTO>>>;
+    public generateEtatFacturationReport(etablissementSourceParam: Array<string>, paramDateDebut: string, paramDateFin: string, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json', context?: HttpContext, transferCache?: boolean}): Observable<any> {
+        if (etablissementSourceParam === null || etablissementSourceParam === undefined) {
+            throw new Error('Required parameter etablissementSourceParam was null or undefined when calling generateEtatFacturationReport.');
+        }
+        if (paramDateDebut === null || paramDateDebut === undefined) {
+            throw new Error('Required parameter paramDateDebut was null or undefined when calling generateEtatFacturationReport.');
+        }
+        if (paramDateFin === null || paramDateFin === undefined) {
+            throw new Error('Required parameter paramDateFin was null or undefined when calling generateEtatFacturationReport.');
+        }
+
+        let localVarQueryParameters = new HttpParams({encoder: this.encoder});
+        if (etablissementSourceParam) {
+            etablissementSourceParam.forEach((element) => {
+                localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
+                  <any>element, 'etablissementSourceParam');
+            })
+        }
+        localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
+          <any>paramDateDebut, 'paramDateDebut');
+        localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
+          <any>paramDateFin, 'paramDateFin');
+
+        let localVarHeaders = this.defaultHeaders;
+
+        // authentication (bearerAuth) required
+        localVarHeaders = this.configuration.addCredentialToHeaders('bearerAuth', 'Authorization', localVarHeaders, 'Bearer ');
+
+        const localVarHttpHeaderAcceptSelected: string | undefined = options?.httpHeaderAccept ?? this.configuration.selectHeaderAccept([
+            'application/json'
+        ]);
+        if (localVarHttpHeaderAcceptSelected !== undefined) {
+            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
+        }
+
+        const localVarHttpContext: HttpContext = options?.context ?? new HttpContext();
+
+        const localVarTransferCache: boolean = options?.transferCache ?? true;
+
+
+        let responseType_: 'text' | 'json' | 'blob' = 'json';
+        if (localVarHttpHeaderAcceptSelected) {
+            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
+                responseType_ = 'text';
+            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
+                responseType_ = 'json';
+            } else {
+                responseType_ = 'blob';
+            }
+        }
+
+        let localVarPath = `/encaissement/etatFacturation`;
+        return this.httpClient.request<Array<EncaissementDTO>>('get', `${this.configuration.basePath}${localVarPath}`,
+            {
+                context: localVarHttpContext,
+                params: localVarQueryParameters,
+                responseType: <any>responseType_,
+                withCredentials: this.configuration.withCredentials,
+                headers: localVarHeaders,
+                observe: observe,
+                transferCache: localVarTransferCache,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * @param etablissementSourceParam 
+     * @param paramDateDebut 
+     * @param paramDateFin 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public generateFichierExcelDeFacturation(etablissementSourceParam: Array<string>, paramDateDebut: string, paramDateFin: string, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: '*/*', context?: HttpContext, transferCache?: boolean}): Observable<string>;
+    public generateFichierExcelDeFacturation(etablissementSourceParam: Array<string>, paramDateDebut: string, paramDateFin: string, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: '*/*', context?: HttpContext, transferCache?: boolean}): Observable<HttpResponse<string>>;
+    public generateFichierExcelDeFacturation(etablissementSourceParam: Array<string>, paramDateDebut: string, paramDateFin: string, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: '*/*', context?: HttpContext, transferCache?: boolean}): Observable<HttpEvent<string>>;
+    public generateFichierExcelDeFacturation(etablissementSourceParam: Array<string>, paramDateDebut: string, paramDateFin: string, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: '*/*', context?: HttpContext, transferCache?: boolean}): Observable<any> {
+        if (etablissementSourceParam === null || etablissementSourceParam === undefined) {
+            throw new Error('Required parameter etablissementSourceParam was null or undefined when calling generateFichierExcelDeFacturation.');
+        }
+        if (paramDateDebut === null || paramDateDebut === undefined) {
+            throw new Error('Required parameter paramDateDebut was null or undefined when calling generateFichierExcelDeFacturation.');
+        }
+        if (paramDateFin === null || paramDateFin === undefined) {
+            throw new Error('Required parameter paramDateFin was null or undefined when calling generateFichierExcelDeFacturation.');
+        }
+
+        let localVarQueryParameters = new HttpParams({encoder: this.encoder});
+        if (etablissementSourceParam) {
+            etablissementSourceParam.forEach((element) => {
+                localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
+                  <any>element, 'etablissementSourceParam');
+            })
+        }
+        localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
+          <any>paramDateDebut, 'paramDateDebut');
+        localVarQueryParameters = this.addToHttpParams(localVarQueryParameters,
+          <any>paramDateFin, 'paramDateFin');
+
+        let localVarHeaders = this.defaultHeaders;
+
+        // authentication (bearerAuth) required
+        localVarHeaders = this.configuration.addCredentialToHeaders('bearerAuth', 'Authorization', localVarHeaders, 'Bearer ');
+
+        const localVarHttpHeaderAcceptSelected: string | undefined = options?.httpHeaderAccept ?? this.configuration.selectHeaderAccept([
+            '*/*'
+        ]);
+        if (localVarHttpHeaderAcceptSelected !== undefined) {
+            localVarHeaders = localVarHeaders.set('Accept', localVarHttpHeaderAcceptSelected);
+        }
+
+        const localVarHttpContext: HttpContext = options?.context ?? new HttpContext();
+
+        const localVarTransferCache: boolean = options?.transferCache ?? true;
+
+
+        let responseType_: 'text' | 'json' | 'blob' = 'json';
+        if (localVarHttpHeaderAcceptSelected) {
+            if (localVarHttpHeaderAcceptSelected.startsWith('text')) {
+                responseType_ = 'text';
+            } else if (this.configuration.isJsonMime(localVarHttpHeaderAcceptSelected)) {
+                responseType_ = 'json';
+            } else {
+                responseType_ = 'blob';
+            }
+        }
+
+        let localVarPath = `/encaissement/fichierExcleDeFacturation`;
+        return this.httpClient.request<string>('get', `${this.configuration.basePath}${localVarPath}`,
+            {
+                context: localVarHttpContext,
+                params: localVarQueryParameters,
+                responseType: <any>responseType_,
+                withCredentials: this.configuration.withCredentials,
+                headers: localVarHeaders,
+                observe: observe,
+                transferCache: localVarTransferCache,
+                reportProgress: reportProgress
+            }
+        );
     }
 
     /**
